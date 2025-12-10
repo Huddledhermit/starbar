9c40fd49dfe583618d1cdc548fb687da43deb7f4
huddledhermit <gkamps@proton.me>
2025-12-10 06:37:05 -0500

    added iced basics to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-   
+use iced;
+
+fn main() -> iced::Result {
+
+}
+enum Message{
+
+}
+#[derive(Default)]
+struct starbar{
+
+}
+impl starbar{
+
+    fn update(&mut self, message: Message){
+        match message{
+
+        }
+
+    }
+    fn view(&self){
+        column![
+
+        ]
+        .into
+    }
+
 }
