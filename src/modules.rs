13493ca8cad2bbc35c8b8aa3b4f24b86ac596570
Gavin Kamps <gkamps@proton.me>
2025-12-12 17:03:58 -0500

    created new function

--- src/modules.rs
+++ src/modules.rs
@@ -5,9 +5,18 @@ use iced;
 
 pub struct Clock {
     text: String,
-    tooltip: String,
+    tooltip: bool,
+    tipvalue: Option<String>,
 }
 impl Clock {
+    fn new(mut self, display: String, tooltip: bool) -> Clock {
+        self.text = display;
+        if self.tooltip == false {
+            self.tipvalue = None
+        } else {
+        };
+        self
+    }
     fn get_time(self) -> String {
         let time_no_format = chrono::Local::now();
         format!("{}", time_no_format.format("%H:%M|%d/%m"))
