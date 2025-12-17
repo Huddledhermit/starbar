6fdb0da1f4b749da465fd8d21eb4c1409605f946
gavin <gkamps@proton.me>
2025-12-17 17:15:01 -0500

    work on config

--- src/configreader.rs
+++ src/configreader.rs
@@ -1,2 +1,7 @@
 use serde::Deserialize;
 use toml::{Table };
+
+fn read_config(file: String){
+    let file = std::fs::read_to_string("starbar/config.toml");
+    let config : Table = file.parse().unwrap();
+}
