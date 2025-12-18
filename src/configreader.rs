9b02d90a1680069d24d549d44331d3cd77c901f1
gavin <gkamps@proton.me>
2025-12-18 11:56:58 -0500

    upadte configreader

--- src/configreader.rs
+++ src/configreader.rs
@@ -4,8 +4,8 @@ use toml::Table;
 //struct to represent the whole config file
 #[derive(Deserialize)]
 struct cfg {
-    colors: string,
-    barinfo: string,
+    colors: String,
+    barinfo: String,
     modules-left:  string,
     modules-right: string,
     modules-center: string,
@@ -21,3 +21,10 @@ fn read_config(file: String) {
 }
 //WIP will parse the info from the config structs to the module structs
 fn config_to_modules(){}
+
+//structs representing configs for individual modules
+struct clock{
+    icon: Option<String>,
+    format: String,
+    tooltip: Option<String>,
+}
