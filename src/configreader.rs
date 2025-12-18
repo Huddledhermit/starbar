29dec0625f0a0af2787b3d7969f9fd2b092dc130
gavin <gkamps@proton.me>
2025-12-18 11:24:22 -0500

    more work on config

--- src/configreader.rs
+++ src/configreader.rs
@@ -1,9 +1,18 @@
-use std::string;
-
 use serde::Deserialize;
 use toml::Table;
+
+#[derive(Deserialize)]
 struct cfg {
     colors: string,
+    barinfo: string,
+    modules-left:  string,
+    modules-right: string,
+    modules-center: string,
+    cpu: string,
+    clock: string,
+    launcher: string,
+    wifi: string
+
 }
 fn read_config(file: String) {
     let file = std::fs::read_to_string("starbar/config.toml");
